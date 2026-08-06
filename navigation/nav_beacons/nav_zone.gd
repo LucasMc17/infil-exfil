c27d5e9a50eb6f6394a91ca452a00931210bfe2a
Lucas McGill <lmlucasmc8@gmail.com>
2026-08-05 20:11:27 -0400

    finished adding navzones to test level 5

--- navigation/nav_beacons/nav_zone.gd
+++ navigation/nav_beacons/nav_zone.gd
@@ -20,6 +20,8 @@ var _debug_meshes : Array[MeshInstance3D]
 
 var _nav_meshes : Array[MeshInstance3D]
 
+var _exit_meshes : Array[MeshInstance3D]
+
 var plane_material : StandardMaterial3D
 
 @export var test_point := Vector2i(0,0):
@@ -49,6 +51,7 @@ func has_point(point : Vector2i) -> bool:
 func _redraw_meshes() -> void:
 	_debug_meshes.clear()
 	_nav_meshes.clear()
+	_exit_meshes.clear()
 
 	for child in get_children():
 		child.queue_free()
@@ -70,7 +73,6 @@ func _redraw_meshes() -> void:
 			add_child(mesh_instance)
 		
 		for point : Vector2i in configs.points:
-			print(point)
 			var mesh_instance = MeshInstance3D.new()
 			var box_mesh = BoxMesh.new()
 			box_mesh.material = POINT_MATERIAL
@@ -81,6 +83,19 @@ func _redraw_meshes() -> void:
 			mesh_instance.mesh = box_mesh
 
 			add_child(mesh_instance)
+		
+		for exit : Vector2i in configs.exits:
+			var mesh_instance = MeshInstance3D.new()
+			var cylinder_mesh = CylinderMesh.new()
+			mesh_instance.position.x = exit.x + 0.5
+			mesh_instance.position.z = exit.y + 0.5
+			_exit_meshes.append(mesh_instance)
+
+			mesh_instance.mesh = cylinder_mesh
+
+			add_child(mesh_instance)
+
+			
 # func _resize() -> void:
 # 	rect = Rect2(Vector2(global_position.x, global_position.z), Vector2(width, depth))
 # 	if _editor_mesh:
--- navigation/nav_beacons/nav_zone_configs.gd
+++ navigation/nav_beacons/nav_zone_configs.gd
@@ -2,6 +2,12 @@
 class_name NavZoneConfigFile
 extends Resource
 
+# @export var base_position : Vector3i:
+# 	set(val):
+# 		base_position = val
+# 		if Engine.is_editor_hint():
+# 			emit_changed()
+
 @export var areas : Array[Rect2i]:
 	set(val):
 		areas = val
@@ -11,5 +17,11 @@ extends Resource
 @export var points : Array[Vector2i]:
 	set(val):
 		points = val
+		if Engine.is_editor_hint():
+			emit_changed()
+
+@export var exits : Array[Vector2i]:
+	set(val):
+		exits = val
 		if Engine.is_editor_hint():
 			emit_changed()
